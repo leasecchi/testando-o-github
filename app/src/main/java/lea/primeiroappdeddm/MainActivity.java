47c448ea7de7063d9bb70a43be89a1d470625b88
leasecchi <leasecchi12@gmail.com>
2021-06-01 23:12:24 -0300

    Initial commit

---
+++ app/src/main/java/lea/primeiroappdeddm/MainActivity.java
@@ -0,0 +1,13 @@
+package lea.primeiroappdeddm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
